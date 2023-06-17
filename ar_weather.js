12bac6dbf0b1106912c3300312918f3a37f657e3
JishnuCode2 <88920598+JishnuCode2@users.noreply.github.com>
2023-06-17 11:23:14 +0530

    Add files via upload

---
+++ ar_weather.js
@@ -0,0 +1,22 @@
+let coordinates = {};
+
+$(document).ready(function(){
+    getCoords()
+});
+
+function getCoords(){
+    let searchParams = new URLSearchParams(window.location.search);
+    if(searchParams.has("source") && searchParams.has("destination")){
+        let source = searchParams.get("source");
+        let destination = searchParams.get("destination");
+
+        coordinates.source_lat = source.split(";")[0];
+        coordinates.source_lng = source.split(";")[1];
+
+        coordinates.destination_lat = destination.split(";")[0];
+        coordinates.destination_lng = destination.split(";")[1];
+    }else{
+        alert("Not Selected the Co-ordinates")
+        window.history.back()
+    }
+};
